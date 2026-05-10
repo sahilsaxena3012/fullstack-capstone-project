d14b444cdbfae4e8900465631f0429e0b264b1d0
sahilsaxena3012 <sahil.saxena3012@gmail.com>
2026-05-10 22:31:47 +0530

    Update searchRoutes.js

--- giftlink-backend/routes/searchRoutes.js
+++ giftlink-backend/routes/searchRoutes.js
@@ -1,4 +1,6 @@
-	const express = require('express');
+/*jshint esversion: 8 */
+
+const express = require('express');
 	const router = express.Router();
 	const connectToDatabase = require('../models/db');
 
